c961496cb9ae6382137c10c509330876b211da13
Jeroen Meulendijks <jeroen.meulendijks@gmail.com>
2018-04-13 16:38:38 +0200

    WIP: get stockstats and bokeh up and running

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ import time
 from model.TimedThread import *
 
 # Custom settings
-LOOP_DURATION = 60 # Time period (in seconds)
-MAX_LOOP_TIME = 120 # 10 * 60 * 60 # Max duration to run (in seconds)
+LOOP_DURATION = 0.1 # Time period (in seconds)
+MAX_LOOP_TIME = 10000 # 10 * 60 * 60 # Max duration to run (in seconds)
 QUOTE_CURRENCY = "BTC" # Cryptocurrency of choice
 BASE_CURRENCY = "EUR" # Fiat currency of choice
 CSV_PRICE = "price.csv" # Price CSV name
@@ -20,10 +20,10 @@ timer_count = 0
 while True:
     if (timer_count >= MAX_LOOP_TIME):
         break
-    thread.getModel().plotGraph();
+
     time.sleep(LOOP_DURATION/4)
     timer_count += 1
 
 stopFlag.set()
-
+thread.getModel().plotGraph()
 input("Press [enter] to continue.")
--- model/Model.py
+++ model/Model.py
@@ -2,7 +2,11 @@
 from datetime import datetime
 import pandas as pd
 import os.path
-import matplotlib.pyplot as plt
+
+from stockstats import StockDataFrame
+from math import pi
+from bokeh.plotting import figure, show, output_notebook, output_file
+from bokeh.models import DatetimeTickFormatter
 
 class Model(object):
     def __init__(self, csv_prices):
@@ -11,11 +15,17 @@ class Model(object):
 
         # Create dataframes to store data
         self.ema_dataframe = pd.DataFrame(data={'datetime': [],'price': [], 'EMA5': [], 'EMA20': [], 'RSI': [], 'signal': []})
+        self.df = pd.DataFrame(data={'datetime': [],'open': [], 'high': [], 'low': [], 'close': [], 'volume': []})
 
         # Add headers to CSV if don't exist
         csv_price_exists = os.path.isfile(self.csv_price)
         #if not csv_price_exists:
         self.logPrice(False)
+
+    def add(self, object):
+        # [ time, low, high, open, close, volume ]
+        time = datetime.fromtimestamp(object[0])
+        self.df = self.df.append(pd.DataFrame({'datetime': time, 'low': object[1], 'high': object[2], 'open': object[3], 'close': object[4], 'volume': object[5]}, index=[0]), ignore_index=True)
 
     def addPrice(self, datetime, price):
         # Get current price and time and add to dataframe
@@ -62,16 +72,82 @@ class Model(object):
         df = self.ema_dataframe.tail(50)
         length = df.shape[0]
         if length > 1:
-            plt.clf()
-            plt.plot(df['datetime'], df['price'])
-            plt.plot(df['datetime'], df['EMA5'])
-            plt.plot(df['datetime'], df['EMA20'])
-            plt.xlabel('Datetime')
-            plt.ylabel('Price')
-            plt.legend()
-            plt.gca().relim()
-            plt.gca().autoscale_view()
-            plt.pause(0.05)
+            dfCopy = self.df.copy(True)
+            stock = StockDataFrame.retype(dfCopy)
+            #rsi = stock['rsi_14']
+            ema5 = stock['high_5_ema']
+            ema20 = stock['high_20_ema']
+
+            signal = stock['macds']        # Your signal line
+            macd   = stock['macd']         # The MACD that need to cross the signal line to give you a Buy/Sell signal
+            listLongShort = ["No data"]    # Since you need at least two days in the for loop
+
+            for i in range(1, len(signal)):
+                # If the MACD crosses the signal line upward BUY!
+                if macd[i] > signal[i] and macd[i - 1] <= signal[i - 1]:
+                    listLongShort.append("BUY")
+                # The other way around. SELL
+                elif macd[i] < signal[i] and macd[i - 1] >= signal[i - 1]:
+                    listLongShort.append("SELL")
+                # Do nothing if not crossed
+                else:
+                    listLongShort.append("HOLD")
+
+            stock['Advice'] = listLongShort
+
+            # The advice column means "Buy/Sell/Hold" at the end of this day or
+            #  at the beginning of the next day, since the market will be closed
+            #print(stock['Advice'])
+
+            output_notebook()
+
+            df_limit = dfCopy.copy()
+            inc = df_limit.close > df_limit.open
+            dec = df_limit.open > df_limit.close
+
+            title = 'TEST PLOTTING'
+            p = figure(x_axis_type="datetime", plot_width=1000, title=title)
+            p.xaxis.formatter=DatetimeTickFormatter(
+                                                    hours=["%d %B %Y"],
+                                                    days=["%d %B %Y"],
+                                                    months=["%d %B %Y"],
+                                                    years=["%d %B %Y"],
+                                                )
+
+            p.line(df_limit.datetime, df_limit.close, color='black')
+
+            # plot macd strategy
+            p.line(df_limit.datetime, 0, color='black')
+            p.line(df_limit.datetime, df_limit.macd, color='blue')
+            p.line(df_limit.datetime, df_limit.macds, color='orange')
+            p.vbar(x=df_limit.datetime, bottom=[0 for _ in df_limit.datetime], top=df_limit.macdh, width=4, color="purple")
+
+            # plot candlesticks
+            candlestick_width = 3600 * 1000 # 1 hour in ms
+            p.segment(df_limit.datetime, df_limit.high, df_limit.datetime, df_limit.low, color="black")
+            p.vbar(df_limit.datetime[inc], candlestick_width, df_limit.open[inc],
+                   df_limit.close[inc], fill_color="#57a52c", line_color="black")
+            p.vbar(df_limit.datetime[dec], candlestick_width, df_limit.open[dec],
+                   df_limit.close[dec], fill_color="#dd0000", line_color="black")
+
+            # Plot buy and sell signals
+            linecolor = []
+            alpha = []
+            for i, a in enumerate(df_limit.Advice):
+                if (a == "BUY"):
+                    alpha.append(1)
+                    linecolor.append("green")
+                elif (a == "SELL"):
+                    alpha.append(1)
+                    linecolor.append("red")
+                else:
+                    alpha.append(0)
+                    linecolor.append("black")
+
+            p.text(df_limit.datetime, df_limit.high + 50, text=df_limit.Advice, text_color=linecolor, text_alpha=alpha, angle=1.0)
+
+            output_file("visualizing_trading_strategy.html", title="visualizing trading strategy")
+            show(p)
 
     def logPrice(self, append = True):
         mode='w'
